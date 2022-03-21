bd607ae757b8a191570654eddaf842fb0fce413e
46kuri_ <ghost.46kuri@gmail.com>
2022-03-21 15:36:27 +0900

    [Add] 破滅的なアップデートを追加

--- src/main/java/com/github/sirokuri_/junksurvival/listeners/JunkSurvivalGiantSpawnListener.java
+++ src/main/java/com/github/sirokuri_/junksurvival/listeners/JunkSurvivalGiantSpawnListener.java
@@ -1,37 +1,40 @@
 package com.github.sirokuri_.junksurvival.listeners;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
+
 
 public class JunkSurvivalGiantSpawnListener implements Listener {
 
+    public int playerDeathCount = 0;
+
     @EventHandler
     public void onDeath(PlayerDeathEvent e) {
-        Location loc = e.getEntity().getLocation();
+        Player player = e.getEntity().getPlayer();
+        if (player == null) return;
+        Location loc = player.getLocation();
         World world = loc.getWorld();
         if (world == null) return;
         if (!world.getName().contains("junkSurvival")) {
             return;
         }
-        loc.getWorld().spawnEntity(loc, EntityType.GIANT);
-    }
-
-    @EventHandler
-    public void onSpawn(EntitySpawnEvent event){
-        Entity entity = event.getEntity();
-        Location location = entity.getLocation();
-        World world = location.getWorld();
-        if (world == null) return;
-        if (world.getName().contains("junkSurvival")) {
-            if (!(entity.getType() == EntityType.GIANT)){
-                event.setCancelled(true);
+        world.spawnEntity(loc, EntityType.GIANT);
+        if (playerDeathCount == 10){
+            for (Player players : Bukkit.getServer().getOnlinePlayers()) {
+                Location location = players.getLocation();
+                World playersWorld = location.getWorld();
+                if(playersWorld == null) return;
+                playersWorld.spawnEntity(location,EntityType.WITHER);
             }
+            playerDeathCount = 0;
+        }else{
+            playerDeathCount++;
         }
     }
 }
