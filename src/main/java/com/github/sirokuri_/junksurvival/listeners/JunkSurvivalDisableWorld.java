c051e8c6c372217b9ba1c0f8d11b6b2fb051a7e3
46kuri_ <ghost.46kuri@gmail.com>
2023-01-10 21:56:52 +0900

    [Fix] 対象ワールド名の修正

--- src/main/java/com/github/sirokuri_/junksurvival/listeners/JunkSurvivalDisableWorld.java
+++ src/main/java/com/github/sirokuri_/junksurvival/listeners/JunkSurvivalDisableWorld.java
@@ -13,7 +13,7 @@ public class JunkSurvivalDisableWorld implements Listener {
     public void onWorld(PlayerPortalEvent event){
         World world = Objects.requireNonNull(event.getTo()).getWorld();
         if (world == null) return;
-        if (world.getName().equalsIgnoreCase("world_nether") || world.getName().equalsIgnoreCase("world_end")){
+        if (world.getName().equalsIgnoreCase("world_nether") || world.getName().equalsIgnoreCase("world_the_end")){
             event.setCancelled(true);
         }
     }
